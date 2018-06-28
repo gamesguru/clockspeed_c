27c20894ccd5a94a6753ce153fd0f2dd5c8accd2
gamesguru <mathmuncher11@gmail.com>
2018-06-28 00:55:00 -0400

    initial commit

---
+++ clockspeeds.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+
+uint64_t rdtsc(void)
+{
+    uint32_t ret0[2];
+    __asm__ __volatile__("rdtsc" : "=a"(ret0[0]), "=d"(ret0[1]));
+    return ((uint64_t)ret0[1] << 32) | ret0[0];
+}
+
+int main(int argc, const char * argv[])
+{
+    uint64_t startCount = rdtsc();
+    sleep(1);
+    uint64_t endCount = rdtsc();
+
+    printf("%lu\n", (endCount - startCount) / 1000);
+    printf("%lu\n", (endCount - startCount));
+    return 0;
+}
